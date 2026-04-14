eca40afe5dff4ef410c04c2fe68533dbb599c3d9
Lakshita <lakshitakhanna46@example.com>
2026-04-14 21:33:17 +0530

    fix static path

--- crafter_lakshita/settings.py
+++ crafter_lakshita/settings.py
@@ -123,6 +123,6 @@ STATICFILES_DIRS = [BASE_DIR / "static"]
 MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "media"
 EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
-STATIC_URL = "/static/"
+
 
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
